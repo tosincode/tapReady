30dce1cfd1a1d3fd9d7e918a6b93a8e5f1a45bb0
olusammy <olumorinsammy@gmail.com>
2023-07-30 19:56:13 +0100

    remove iphone svg in landscape mode

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,35 +3,56 @@ import { useWindowSize } from "usehooks-ts";
 import React from "react";
 import SvgIcon from "./SvgIcon/SvgIcon";
 import Nav from "./Nav";
+import useScreenOrientation from "react-hook-screen-orientation";
 
 const Hero = () => {
-  const { width } = useWindowSize();
+  const { width, height } = useWindowSize();
+  const screenOrientation = useScreenOrientation();
 
   const isMobile = width < 978;
+  const sm = width < 500;
 
   return (
     <div className="bg-black w-screen relative h-screen overflow-hidden">
       <div className="absolute text-white w-screen z-[99999999] ">
         <Nav />
-
         <p className="text-6xl max-md:text-xl text-center mt-10 ">
           Accept Contactless payment <br />
           on NFC-enabled{" "}
           <span className="font-semibold text-gradient">devices</span>
         </p>
       </div>
-      <div className="absolute bottom-0 iphone z-[99999999]">
+      <div
+        className={`absolute bottom-0 iphone z-[99999999] ${
+          screenOrientation === "landscape-primary" && sm ? "hidden" : ""
+        } `}
+      >
         <div className=" ">
           <div className="absolute lg:scale-[1.5]  z-[999] top-7 ">
             <SvgIcon iconName="contactlessglass" />
           </div>
-          <div className={`absolute ${isMobile ? "-right-16 top-24" : "-right-52 top-40"} `}>
+          <div
+            className={`absolute ${
+              isMobile ? "-right-16 top-24" : "-right-52 top-40"
+            } `}
+          >
             <SvgIcon iconName="card" width={isMobile ? 150 : undefined} />
           </div>
-          <div className={`absolute ${isMobile ? "top-[90px] -left-20" : "top-[190px] -left-28"}`}>
-            <SvgIcon iconName="androidGlass" width={isMobile ? 140 : undefined} />
+          <div
+            className={`absolute ${
+              isMobile ? "top-[90px] -left-20" : "top-[190px] -left-28"
+            }`}
+          >
+            <SvgIcon
+              iconName="androidGlass"
+              width={isMobile ? 140 : undefined}
+            />
           </div>
-          <div className={`absolute ${isMobile ? "top-[150px] -left-20" : "top-[300px] -left-28"}`}>
+          <div
+            className={`absolute ${
+              isMobile ? "top-[150px] -left-20" : "top-[300px] -left-28"
+            }`}
+          >
             <SvgIcon iconName="iosGlass" width={isMobile ? 140 : undefined} />
           </div>
           <SvgIcon iconName="iphone" width={isMobile ? 400 : undefined} />
