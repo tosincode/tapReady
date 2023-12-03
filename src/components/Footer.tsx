2933b063a4e9e4a9c71e867709b9866043d5ee52
Tosin Falodun <52760577+tosincode@users.noreply.github.com>
2023-12-03 23:50:13 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,16 @@ const Footer = () => {
   return (
     <div className="bg-black py-16">
       <div className="container text-white flex flex-col lg:flex-row items-center mx-auto justify-between ">
-        <SvgIcon iconName="logo" />
+{/*         <SvgIcon iconName="logo" /> */}
+        <Image
+            src="/images/logo-tapready.png"
+            alt="TapReadyLogo "
+            width={130}
+            height={100}
+            className="z-50"
+          />
+          {/* <SvgIcon iconName="logo" width={width < 600 ? 100 : undefined} /> */}
+        
         <p className="mt-6 lg:mt-0" >copyright &copy; {new Date().getFullYear()}. All Rights Reserved</p>
       </div>
     </div>
